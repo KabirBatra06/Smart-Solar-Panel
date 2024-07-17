a1246832a89126e1d5640a7aaedfed42cd412ee1
Kabir Batra <kabirbatra06@gmail.com>
2024-07-17 20:45:26 +0530

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -74,97 +74,12 @@ double getMeanAnomalyMoon(double);
 double getMoonLatitudeArgument(double);
 double getMoonAscendingNode(double);
 
-struct L0
+struct triple
 {
     long a;
     double b;
     double c;
 };
-
-struct L1
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct L2
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct L3
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct L4
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct L5
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct B0
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct B1
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct R0
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct R1
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct R2
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct R3
-{
-    long a;
-    double b;
-    double c;
-};
-
-struct R4
-{
-    long a;
-    double b;
-    double c;
-};
-
 
 double getDayinDecimals(int day, double hour, double min, double sec)
 {
@@ -218,9 +133,9 @@ double getJulianEphemerisMillenium(double julianEphemerisCentury)
 }
 
 
-struct L0 *initL0()
+struct triple *initL0()
 {
-    static struct L0 data[64] = {
+    static struct triple data[64] = {
         {175347046, 0, 0},
         {3341656, 4.6692568, 6283.07585},
         {34894, 4.6261, 12566.1517},
@@ -290,9 +205,9 @@ struct L0 *initL0()
     return data;
 }
 
-struct L1 *initL1()
+struct triple *initL1()
 {
-    static struct L1 data[34] = {
+    static struct triple data[34] = {
         {628331966747, 0, 0},
         {206059, 2.678235, 6283.07585},
         {4303, 2.6351, 12566.1517},
@@ -333,9 +248,9 @@ struct L1 *initL1()
 }
 
 
-struct L2 *initL2()
+struct triple *initL2()
 {
-    static struct L2 data[20] = {
+    static struct triple data[20] = {
         {52919, 0, 0},
         {8720, 1.0721, 6283.0758},
         {309, 0.867, 12566.152},
@@ -361,9 +276,9 @@ struct L2 *initL2()
     return data;
 }
 
-struct L3 *initL3()
+struct triple *initL3()
 {
-    static struct L3 data[7] = {
+    static struct triple data[7] = {
         {289, 5.844, 6283.076},
         {35, 0, 0},
         {17, 5.49, 12566.15},
@@ -376,9 +291,9 @@ struct L3 *initL3()
     return data;
 }
 
-struct L4 *initL4()
+struct triple *initL4()
 {
-    static struct L4 data[3] = {
+    static struct triple data[3] = {
         {114, 3.142, 0},
         {8, 4.13, 6283.08},
         {1, 3.84, 12566.15}
@@ -387,16 +302,16 @@ struct L4 *initL4()
     return data;
 }
 
-struct L5 *initL5()
+struct triple *initL5()
 {
-    static struct L5 data[1] = {
+    static struct triple data[1] = {
         {1, 3.14, 0}
     };
 
     return data;
 }
 
-double getL0(struct L0 *data, double julianEphemerisMillenium)
+double getL0(struct triple *data, double julianEphemerisMillenium)
 {
     double L0 = 0;
     for (int i = 0; i < 64; i++)
@@ -406,7 +321,7 @@ double getL0(struct L0 *data, double julianEphemerisMillenium)
     return L0;
 }
 
-double getL1(struct L1 *data, double julianEphemerisMillenium)
+double getL1(struct triple *data, double julianEphemerisMillenium)
 {
     double L1 = 0;
     for (int i = 0; i < 34; i++)
@@ -416,7 +331,7 @@ double getL1(struct L1 *data, double julianEphemerisMillenium)
     return L1;
 }
 
-double getL2(struct L2 *data, double julianEphemerisMillenium)
+double getL2(struct triple *data, double julianEphemerisMillenium)
 {
     double L2 = 0;
     for (int i = 0; i < 20; i++)
@@ -426,7 +341,7 @@ double getL2(struct L2 *data, double julianEphemerisMillenium)
     return L2;
 }
 
-double getL3(struct L3 *data, double julianEphemerisMillenium)
+double getL3(struct triple *data, double julianEphemerisMillenium)
 {
     double L3 = 0;
     for (int i = 0; i < 7; i++)
@@ -436,7 +351,7 @@ double getL3(struct L3 *data, double julianEphemerisMillenium)
     return L3;
 }
 
-double getL4(struct L4 *data, double julianEphemerisMillenium)
+double getL4(struct triple *data, double julianEphemerisMillenium)
 {
     double L4 = 0;
     for (int i = 0; i < 3; i++)
@@ -446,7 +361,7 @@ double getL4(struct L4 *data, double julianEphemerisMillenium)
     return L4;
 }
 
-double getL5(struct L5 *data, double julianEphemerisMillenium)
+double getL5(struct triple *data, double julianEphemerisMillenium)
 {
     double L5 = 0;
     for (int i = 0; i < 1; i++)
@@ -477,9 +392,9 @@ double getHeliocentricLongitude(double L0, double L1, double L2, double L3, doub
     return heliocentric_longitude;
 }
 
-struct B0 *initB0()
+struct triple *initB0()
 {
-    static struct B0 data[5] = {
+    static struct triple data[5] = {
         {280, 3.199, 84334.662},
         {102, 5.422, 5507.553},
         {80, 3.88, 5223.69},
@@ -490,9 +405,9 @@ struct B0 *initB0()
     return data;
 }
 
-struct B1 *initB1()
+struct triple *initB1()
 {
-    static struct B1 data[2] = {
+    static struct triple data[2] = {
         {9, 3.9, 5507.55},
         {6, 1.73, 5223.69}
     };
@@ -500,7 +415,7 @@ struct B1 *initB1()
     return data;
 }
 
-double getB0(struct B0 *data, double julianEphemerisMillenium)
+double getB0(struct triple *data, double julianEphemerisMillenium)
 {
     double B0 = 0;
     for (int i = 0; i < 5; i++)
@@ -510,7 +425,7 @@ double getB0(struct B0 *data, double julianEphemerisMillenium)
     return B0;
 }
 
-double getB1(struct B1 *data, double julianEphemerisMillenium)
+double getB1(struct triple *data, double julianEphemerisMillenium)
 {
     double B1 = 0;
     for (int i = 0; i < 2; i++)
@@ -530,9 +445,9 @@ double getHeliocentricLatitude(double B0, double B1, double JME)
 }
 
 
-struct R0 *initR0()
+struct triple *initR0()
 {
-    static struct R0 data[40] = {
+    static struct triple data[40] = {
         {100013989, 0, 0},
         {1670700, 3.0984635, 6283.07585},
         {13956, 3.05525, 12566.1517},
@@ -579,9 +494,9 @@ struct R0 *initR0()
 }
 
 
-struct R1 *initR1()
+struct triple *initR1()
 {
-    static struct R1 data[10] = {
+    static struct triple data[10] = {
         {103019, 1.10749, 6283.07585},
         {1721, 1.0644, 12566.1517},
         {702, 3.142, 0},
@@ -597,9 +512,9 @@ struct R1 *initR1()
     return data;
 }
 
-struct R2 *initR2()
+struct triple *initR2()
 {
-    static struct R2 data[6] = {
+    static struct triple data[6] = {
         {4359, 5.7846, 6283.0758},
         {124, 5.579, 12566.152},
         {12, 3.14, 0},
@@ -612,9 +527,9 @@ struct R2 *initR2()
 }
 
 
-struct R3 *initR3()
+struct triple *initR3()
 {
-    static struct R3 data[2] = {
+    static struct triple data[2] = {
         {145, 4.273, 6283.076},
         {7, 3.92, 12566.15}
     };
@@ -622,16 +537,16 @@ struct R3 *initR3()
     return data;
 }
 
-struct R4 *initR4()
+struct triple *initR4()
 {
-    static struct R4 data[1] = {
+    static struct triple data[1] = {
         {4, 2.56, 6283.08}
     };
 
     return data;
 }
 
-double getR0(struct R0 *data, double julianEphemerisMillenium)
+double getR0(struct triple *data, double julianEphemerisMillenium)
 {
     double R0 = 0;
     for (int i = 0; i < 40; i++)
@@ -641,7 +556,7 @@ double getR0(struct R0 *data, double julianEphemerisMillenium)
     return R0;
 }
 
-double getR1(struct R1 *data, double julianEphemerisMillenium)
+double getR1(struct triple *data, double julianEphemerisMillenium)
 {
     double R1 = 0;
     for (int i = 0; i < 10; i++)
@@ -651,7 +566,7 @@ double getR1(struct R1 *data, double julianEphemerisMillenium)
     return R1;
 }
 
-double getR2(struct R2 *data, double julianEphemerisMillenium)
+double getR2(struct triple *data, double julianEphemerisMillenium)
 {
     double R2 = 0;
     for (int i = 0; i < 6; i++)
@@ -661,7 +576,7 @@ double getR2(struct R2 *data, double julianEphemerisMillenium)
     return R2;
 }
 
-double getR3(struct R3 *data, double julianEphemerisMillenium)
+double getR3(struct triple *data, double julianEphemerisMillenium)
 {
     double R3 = 0;
     for (int i = 0; i < 2; i++)
@@ -671,7 +586,7 @@ double getR3(struct R3 *data, double julianEphemerisMillenium)
     return R3;
 }
 
-double getR4(struct R4 *data, double julianEphemerisMillenium)
+double getR4(struct triple *data, double julianEphemerisMillenium)
 {
     double R4 = 0;
     for (int i = 0; i < 1; i++)
@@ -1067,12 +982,12 @@ double *caller(int day, int year, int month, float hour, float min, float sec, f
     printf("Julian Ephemeris Millenium = %lf\n", julianEphemerisMillenium);
     */
 
-    struct L0 *dataL0 = initL0();
-    struct L1 *dataL1 = initL1();
-    struct L2 *dataL2 = initL2();
-    struct L3 *dataL3 = initL3();
-    struct L4 *dataL4 = initL4();
-    struct L5 *dataL5 = initL5();
+    struct triple *dataL0 = initL0();
+    struct triple *dataL1 = initL1();
+    struct triple *dataL2 = initL2();
+    struct triple *dataL3 = initL3();
+    struct triple *dataL4 = initL4();
+    struct triple *dataL5 = initL5();
 
     double L0 = getL0(dataL0, julianEphemerisMillenium);
     double L1 = getL1(dataL1, julianEphemerisMillenium);
@@ -1085,8 +1000,8 @@ double *caller(int day, int year, int month, float hour, float min, float sec, f
 
     // printf("Heliocentric Longitude = %lf\n", heliocentric_longitude);
 
-    struct B0 *dataB0 = initB0();
-    struct B1 *dataB1 = initB1();
+    struct triple *dataB0 = initB0();
+    struct triple *dataB1 = initB1();
 
     double B0 = getB0(dataB0, julianEphemerisMillenium);
     double B1 = getB1(dataB1, julianEphemerisMillenium);
@@ -1095,11 +1010,11 @@ double *caller(int day, int year, int month, float hour, float min, float sec, f
 
     // printf("Heliocentric Latitude = %lf\n", heliocentric_latitude);
 
-    struct R0 *dataR0 = initR0();
-    struct R1 *dataR1 = initR1();
-    struct R2 *dataR2 = initR2();
-    struct R3 *dataR3 = initR3();
-    struct R4 *dataR4 = initR4();
+    struct triple *dataR0 = initR0();
+    struct triple *dataR1 = initR1();
+    struct triple *dataR2 = initR2();
+    struct triple *dataR3 = initR3();
+    struct triple *dataR4 = initR4();
 
     double R0 = getR0(dataR0, julianEphemerisMillenium);
     double R1 = getR1(dataR1, julianEphemerisMillenium);
